df82fbe1130fac5cb0a6707391b96f93410a7471
Android Studio <devandroid@ort>
2025-05-09 10:20:04 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'; 
+import React, { useState } from 'react';
 
-export default function App() {
+export default function App() { 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor='#0000ff' /> 
+      <Text>Email:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ingresa tu correo"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        onChangeText={setEmail}
+        value={email}
+      />
+
+      <Text>Contraseña:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ingresa tu contraseña"
+        secureTextEntry={true}
+        onChangeText={setPassword}
+        value={password}
+      />
+       <View style={styles.buttonContainer}>
+        <Button title="Ingresar" onPress={handleLogin} color="#0000FF" />
+      </View>
     </View>
   );
 }
@@ -16,5 +39,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  }, 
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'blue', 
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  }, 
+  buttonContainer: {
+    width: '100%',
+    marginTop: 10,
   },
 });
